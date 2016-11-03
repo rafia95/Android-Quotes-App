5b6e5bed1b08203468d677a59ea1e57a9b4156f6
Denys Melyukhov <denys.melyukhov@gmail.com>
2016-11-03 23:34:34 +0000

    Added QuoteActivity (No intents yet, no category)

---
+++ app/src/main/java/com/example/rafia/qdmraquoteapp/QuoteActivity.java
@@ -0,0 +1,77 @@
+package com.example.rafia.qdmraquoteapp;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * @author Denys Melyukhov
+ * @version 1.0.0
+ */
+public class QuoteActivity extends BaseClass
+{
+    private TextView category, attribute, quote, date, reference;
+    private Quote quoteBean;
+
+    /**
+     * Overridden onCreate method.
+     * Binds the TextViews and sets their content.
+     * @param savedInstanceState Bundle
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        category = (TextView)findViewById(R.id.cat);
+        attribute = (TextView)findViewById(R.id.att);
+        quote = (TextView)findViewById(R.id.quote);
+        date = (TextView)findViewById(R.id.date);
+        reference = (TextView)findViewById(R.id.ref);
+
+        //Intent i = getIntent();
+        //quoteBean = (Quote) i.getSerializableExtra("data");
+
+        //category.setText(quoteBean.getCategory());
+        attribute.setText(quoteBean.getAttributed());
+        quote.setText(quoteBean.getQuote());
+        date.setText(quoteBean.getDate());
+        reference.setText(quoteBean.getReference());
+    }
+
+    /**
+     * Event handler for an onClick event on the person the quote is attributed to.
+     * Shows additional information (Blurb) about the person.
+     * @param view TextView
+     */
+    public void onAttributed(View view)
+    {
+        new AlertDialog.Builder(this).setMessage(quoteBean.getBlurb()).setTitle("Blurb")
+                .setCancelable(true).show();
+    }
+
+    /**
+     * Event handler for an onClick event on the reference's URL. Opens the web browser to show
+     * the reference.
+     * @param view TextView
+     */
+    public void onRef(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(quoteBean.getReference()));
+        startActivity(browserIntent);
+    }
+
+    /**
+     * Event handler for the Dismiss button. Closes the activity.
+     * @param view Button
+     */
+    public void onDismiss(View view)
+    {
+        finish();
+    }
+}
